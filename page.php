31f39575a4bceea566c1b02ab8475636c32e07f8
Mariem Jerbi <ton-email@gmail.com>
2026-04-22 14:22:26 +0100

    Design premium et minimaliste pour la page panier

--- page.php
+++ page.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 
-<main id="customer_details" class="site-main container py-24 mx-auto px-6">
-    <div class="woocommerce">
-        <?php while (have_posts()) : the_post(); the_content(); endwhile; ?>
+<main id="customer_details" class="site-main py-32 bg-[#FDF9F6] min-h-screen">
+    <div class="max-w-[1100px] mx-auto px-6">
+        <div class="woocommerce">
+            <?php while (have_posts()) : the_post(); the_content(); endwhile; ?>
+        </div>
     </div>
 </main>
 
--- woocommerce.php
+++ woocommerce.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 
-<div id="customer_details" class="container py-24 mx-auto px-6">
-    <div class="woocommerce">
-        <?php woocommerce_content(); ?>
+<div id="customer_details" class="py-32 bg-[#FDF9F6] min-h-screen">
+    <div class="max-w-[1100px] mx-auto px-6">
+        <div class="woocommerce">
+            <?php woocommerce_content(); ?>
+        </div>
     </div>
 </div>
 
