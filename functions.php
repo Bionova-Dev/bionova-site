5939737a1a5712a6f80d43491731c63728ac6991
Mariem Jerbi <ton-email@gmail.com>
2026-05-05 16:51:03 +0100

    HOTFIX: Empty functions.php

--- functions.php
+++ functions.php
@@ -1,13 +1,3 @@
 <?php
-/**
- * Bionova Theme Functions - EMERGENCY BAREBONES VERSION
- * All complex logic has been removed to restore site accessibility.
- */
-
-add_action( 'after_setup_theme', function() {
-    add_theme_support( 'woocommerce' );
-    add_theme_support( 'post-thumbnails' );
-} );
-
-// Basic WooCommerce support
-add_filter( 'woocommerce_is_purchasable', '__return_true' );
+// Bionova - Cleanest functions.php
+// Empty to diagnose fatal error.
