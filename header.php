b1acae2d08f59825c9d5a4f69a4be4041dbeb2f0
Mariem Jerbi <ton-email@gmail.com>
2026-05-11 16:20:22 +0100

    Exact match for wc_get_cart_url per user request

--- header.php
+++ header.php
@@ -117,7 +117,7 @@
             </a>
 
             <!-- Panier -->
-            <a href="<?php echo function_exists('wc_get_cart_url') ? esc_url( wc_get_cart_url() ) : get_permalink( get_page_by_path( 'panier' ) ); ?>" class="relative p-3 sm:p-4 rounded-2xl text-black hover:text-[#be123c] hover:bg-gray-50 transition-all group" title="Voir le panier">
+            <a href="<?php echo wc_get_cart_url(); ?>" class="relative p-3 sm:p-4 rounded-2xl text-black hover:text-[#be123c] hover:bg-gray-50 transition-all group" title="Voir le panier">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 lg:h-7 lg:w-7 group-hover:scale-110 transition-transform" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
